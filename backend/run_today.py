148f2ad96a186a720be19341cf4ec5b3ad25afc9
saurabh <saurabh141997@gmail.com>
2026-09-08 18:44:19 +0530

    chore: remove temporary backend scripts

--- backend/run_today.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from core.database import init_db
-from api.reviews import run_autonomous_pipeline
-
-async def main():
-    await init_db()
-    await run_autonomous_pipeline()
-
-asyncio.run(main())
